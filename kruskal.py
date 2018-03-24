83109591e917b55b18489fe6e04b1818155b66b3
Harrison Welch <hwelch1@my.apsu.edu>
2018-03-23 22:29:23 -0500

    working on Kruskals

---
+++ kruskal.py
@@ -0,0 +1,16 @@
+import numpy
+import sys
+import random
+
+def kruskal (n, m, E):
+    i = 0
+    j = 0
+    p = 0
+    q = 0
+    e = (0,0)
+    # Sort the me edges in E by weight in nondecreasing order;
+    F = []
+
+
+# main
+
