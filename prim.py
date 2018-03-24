97662e7b43baae8a07828e8e6138876369cdbb99
Harrison Welch <hwelch1@my.apsu.edu>
2018-03-23 20:20:40 -0500

    built the main function, time to debug

--- prim.py
+++ prim.py
@@ -11,9 +11,30 @@ def prim(n, W):
     
     nearest = []
     distance = []
+    F = []
     for i in range (0,n):
-        nearest[i] = 1
-        distance[i] = i
+        print "i = ",i
+        nearest.append(1)
+        distance.append(W[0][i])
+
+    
+
+    for x in range (0,n-1):
+
+        min = sys.maxsize
+        for i in range(1,n):
+            if ( 0 <= distance[i] and distance[i] < min):
+                min = distance[i]
+                vnear = i
+        e = ( vnear, nearest[vnear])
+        F.append(e)
+        distance[vnear] = -1
+        for i in range(1,n):
+            if ( W[i][vnear] < distance[i] ):
+                distance[i] = W[i][vnear]
+                nearest[i] = vnear
+    return W
+
 
 # ======= MAIN ============================
 
@@ -33,5 +54,9 @@ for i in range(0,n):
     W.append(row)
     
     row = []
+
+print "W = ", W
+
+W = prim(n,W)
 
 print "W = ", W
