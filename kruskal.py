b5e43c997f9a974ec377399f2d824b953668ccd2
SUPERharrison <hwelch1@my.apsu.edu>
2018-04-22 14:02:15 -0500

    kruskals working sometimes

--- kruskal.py
+++ kruskal.py
@@ -1,6 +1,21 @@
 import numpy as np
 import sys
 import random
+import time
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-s", action="store_true", dest="stock")
+parser.add_option("-v", action="store_true", dest="verbose")
+parser.add_option("-m", action="store_true", dest="matrix")
+parser.add_option("-e", action="store_true", dest="edge_list")
+parser.add_option("-d", action="store_true", dest="distance")
+parser.add_option("-c", action="store_true", dest="clock")
+parser.add_option("-f", action="store_true", dest="file")
+parser.add_option("-r", "--random", action="store", type="int", dest="random", help="randomizes edge weights for the adjancency matrix")
+parser.add_option("-l", "--limit", action="store", type="int", dest="random_limit", help="upper limit to the randomizer")
+
+(options, args) = parser.parse_args()
 
 def kruskal(n, m, E):
     i = 0
@@ -50,6 +65,7 @@ def kruskal(n, m, E):
             vert_list = appendSet(i, j, vert_list)
             # print "F_list = ", F_list
             # print "vert_list : ", str(vert_list)
+            F_mat[i][j] = e[2]
 
         k = k + 1
 
@@ -57,6 +73,7 @@ def kruskal(n, m, E):
 
     print "E = ", E
     print "F_list = ", F_list
+    print "F_mat = ", F_mat
 
     return F_list, F_mat
 
@@ -101,6 +118,7 @@ def isPromising(i, j, vert_list):
 
 def appendSet(i, j, vert_list):
     theSet = [i, j]
+    print "theSet  = ", theSet
     if len(vert_list) == 0:
         vert_list.append(theSet)
         return vert_list
@@ -111,27 +129,35 @@ def appendSet(i, j, vert_list):
 
     for x in range(0, len(vert_list)):
         for y in range(0, (len(vert_list[x])-1)):
+            print "outer y = ", y
             for z in range (0, len(vert_list[x])):
-                a = vert_list[y][z]
-                print "a = ",a
+                print "y = ",y
+                print "z = ",z
                 print "i = ",i
                 print "j = ",j
+                a = vert_list[y][z]
+                print "a = ",a
+                print ""
                 if a == i:
-                    print "set(vert_list[y]) = ", set(vert_list[y])
-                    print "set(vert_list[y]).union(set(theSet)) = ", set(vert_list[y]).union(set(theSet))
+                    # print "set(vert_list[y]) = ", set(vert_list[y])
+                    # print "set(vert_list[y]).union(set(theSet)) = ", set(vert_list[y]).union(set(theSet))
                     vert_list[x] = (list(set(vert_list[y]).union(set(theSet))))
+
+                    print "1 after vert_list = ", vert_list
                     return vert_list
                 if a == j:
-                    print "set(vert_list[y]) = ", set(vert_list[y])
-                    print "set(vert_list[y]).union(set(theSet)) = ", set(vert_list[y]).union(set(theSet))
+                    # print "set(vert_list[y]) = ", set(vert_list[y])
+                    # print "set(vert_list[y]).union(set(theSet)) = ", set(vert_list[y]).union(set(theSet))
                     vert_list[x] = (list(set(vert_list[y]).union(set(theSet))))
+
+                    print "2 after vert_list = ", vert_list
                     return vert_list
 
             print "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
 
     vert_list.append(theSet)
 
-    print "after vert_list = ", vert_list
+    print "3 after vert_list = ", vert_list
             
     return vert_list
 
@@ -141,15 +167,32 @@ if __name__ == "__main__":
 
     inf = sys.maxint
 
-    n = 5
+    if options.random:
+        n = options.random
+        random_limit = 10
+        if options.random_limit:
+            random_limit = options.random_limit
+        W = []
+        row = []
+        for i in range(0,n):
+            for j in range(0,n):
+                row.append(0)
+            W.append(row)
+            row = []
+        for i in range(0,n):
+            for j in range(i+1,n):
+                W [i][j] = random.randint(1,random_limit)
+    else:
 
-    W = [   
-            [0,3,inf,11,inf],
-            [3,0,12,6,9],
-            [inf,12,0,4,4],
-            [11,6,4,0,2],
-            [inf,9,4,2,0]
-        ]
+        n = 5
+
+        W = [   
+                [0,3,inf,11,inf],
+                [3,0,12,6,9],
+                [inf,12,0,4,4],
+                [11,6,4,0,2],
+                [inf,9,4,2,0]
+            ]
     
     print "W = \n", np.squeeze(np.asarray(W))
 
@@ -161,5 +204,13 @@ if __name__ == "__main__":
     m = len(get_edge_list(W))
 
     K_list, K_mat = kruskal(n, m, F_list)
+
+    # mirror about diagonal
+    for i in range(0, n):
+        for j in range (0, n):
+            K_mat[j][i] = K_mat[i][j]
+
+    print "K_list = ", K_list
+    print "K_mat = \n", np.squeeze(np.asarray(K_mat))
     
     # F_list, F_mat = kruskal(n)
